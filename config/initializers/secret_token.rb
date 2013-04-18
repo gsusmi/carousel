a2d1138aa1cdd1604501af0a1b82e7e1c500ccf7
gsusmi <gsusmi@gmail.com>
2013-04-18 09:44:53 -0400

    clean rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carousel::Application.config.secret_token = '85b88d19d019b665dac67c435dadcbd16ab71905ca39d3f018d08a566ceced362242bab3eb1e1c1f21bf3850a2581131e6abeac07ad31d9088977974cbc8a3a9'
